ae7cc021abd68ba650981c01e2fdc02f875950b2
phongnguyen39 <phong.nguyen39@gmail.com>
2019-09-10 20:51:35 -0700

    updated the problem with instructions

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 arr = [[1,2],[6,8],[14,16],[4,7]]
 function detectOverlap(arr){
+    // access each element
+    // find the min and max of all elements
+    
+    // merge all subarrays, sort the items.  If there is a number that falls out of the min max then false
+
+
+    // return true or false
+//     if there is an over lap in those start and end times
+// so that should return false
+// cause 4 7 and 6 8 overlap
     
 }
